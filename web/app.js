9634ffac0c844a55383d2180d52d88461f426579
tinchoforever <tinchoforever@gmail.com>
2016-10-08 17:34:35 -0300

    golazo AGUANTE METALLICA

--- web/app.js
+++ web/app.js
@@ -3,38 +3,38 @@ var plano01_ent;
 var plano02_ent;
 var plano03_ent;
 
-// var fotos;
-// var fotos_ent = [];
+var fotos;
+var fotos_ent = [];
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     var fotos = document.getElementsByClassName("foto");
-//     console.log(fotos);
-//     console.log(fotos.length);
+document.addEventListener("DOMContentLoaded", function() {
+    var fotos = document.getElementsByClassName("foto");
+    console.log(fotos);
+    console.log(fotos.length);
 
-//     for (var i = 0; i < fotos.length; i++) {
-//         fotos_ent.push(fotos[i]);
-//     }
-//     render();
-// });
+    for (var i = 0; i < fotos.length; i++) {
+        fotos_ent.push(fotos[i]);
+    }
+    render();
+});
 
-// var radio = 2;
+var radio = 2;
 
-// function render(){
-//     requestAnimationFrame(render);
-//     u_time++;
-//     //----------------------------------------------------------//
+function render(){
+    requestAnimationFrame(render);
+    u_time++;
+    //----------------------------------------------------------//
 
 
-//     for (var i = 0; i < fotos_ent.length; i++) {
-//         fotos_ent[i].setAttribute('position', {
-//             x: Math.sin(u_time*0.001+i*10)*radio,
-//             // y: 1.60,
-//             y: 1.60+Math.sin(u_time*0.01+i+10)*0.4,
-//             z: Math.cos(u_time*0.001+i*10)*radio + Math.sin(u_time*0.01+i*10)*0.4
-//         });
-//         fotos_ent[i].setAttribute('look-at', {x:0,y:1.6,z:0});
-//     }
-// }
+    for (var i = 0; i < fotos_ent.length; i++) {
+        fotos_ent[i].setAttribute('position', {
+            x: Math.sin(u_time*0.001+i*10)*radio,
+            // y: 1.60,
+            y: 1.60+Math.sin(u_time*0.01+i+10)*0.4,
+            z: Math.cos(u_time*0.001+i*10)*radio + Math.sin(u_time*0.01+i*10)*0.4
+        });
+        fotos_ent[i].setAttribute('look-at', {x:0,y:1.6,z:0});
+    }
+}
 
 var createPlayerFor = function(selector){
 	var playSelector = selector + 'Play';
